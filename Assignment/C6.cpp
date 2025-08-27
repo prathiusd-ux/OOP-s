ba83846c547678353f1312867602bc2a0d9190f4
prathiusd-ux <prathiusd@gmail.com>
2025-08-27 10:32:35 +0300

    Add Car class with setter/getter and array of cars

---
+++ Assignment/C6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car{
+    private:
+    string brand;
+    int model;
+    public:
+    void set_values(string brnd, int mod){
+        brand=brnd;
+        model=mod;
+    }
+    void get_values(void){
+        cout<<"Brand: "<<brand<<" model: "<< model;
+    }
+
+};
+int main(){
+    Car CarA[2];
+    CarA[0].set_values("Honda CR-V", 2016);
+    CarA[1].set_values("Toyota Corolla", 2019);
+    for(int i=0; i<2;i++){
+        CarA[i].get_values();
+    }
+}
